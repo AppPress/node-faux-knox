4675d65d6fd097b9721989b8e9bd8071aff7e5dc
wlaurance <w.laurance@gmail.com>
2013-07-15 20:41:29 -0400

    Init testing framwork

---
+++ test.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+    http = require('http'),
+    app = express(),
+    should = require('should'),
+    async = require('async'),
+    knox = require('./index');
+
+describe('Faux-Knox', function(){
+  describe('API', function(){
+    
+  });
+});
